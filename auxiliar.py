83a2f08287259f9cee0864a8bc4b1bfd2527632d
Nobr3c <jnobr3c+github@gmail.com>
2024-01-09 17:03:07 -0300

    :books: Aula 1 - Jornada Python - Automação de Tarefas

--- auxiliar.py
+++ auxiliar.py
@@ -2,4 +2,4 @@ import pyautogui
 import time
 
 time.sleep(5)
-print(pyautogui.position())
+print(pyautogui.position)
--- codigo.py
+++ codigo.py
@@ -18,7 +18,7 @@ pyautogui.PAUSE = 0.3
     #aperta a tecla do windows(command + barra de espaço)
 pyautogui.press("win")
     #digita o nome do programa (chrome)
-pyautogui.write("chrome ")
+pyautogui.write("edge ")
     # aperta enter
 pyautogui.press("enter")
 
@@ -37,7 +37,7 @@ time.sleep(5)
 
 # Passo 2: Fazer login    
     # selecionar o campo de email
-pyautogui.click(x=685, y=451)
+pyautogui.click(x=500, y=357)
 
     # digitar o e-mail
 pyautogui.write("pythonimpressionador@gmail.com")
@@ -49,12 +49,45 @@ pyautogui.press("tab")
 pyautogui.write("sua senha")
 
     # clique no botao de login
-pyautogui.click(x=955, y=638) 
+pyautogui.click(x=733, y=687) 
 
 time.sleep(3)
 
-
-
 # Passo 3: Importar a base de produtos pra cadastrar
+    # instalar biblioteca comando pip install pandas numpy openpyxl
+
+import pandas as pd
+
+tabela = pd.read_csv("produtos.csv")
+print(tabela)
+
 # Passo 4: Cadastrar um produto
+for linha in tabela.index:
+    # clicar no campo de código
+    pyautogui.click(x=653, y=294)
+    # pegar da tabela o valor do campo que a gente quer preencher
+    codigo = tabela.loc[linha, "codigo"]
+    # preencher o campo
+    pyautogui.write(str(codigo))
+    # passar para o proximo campo
+    pyautogui.press("tab")
+    # preencher o campo
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    if not pd.isna(obs):
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
+    pyautogui.press("tab")
+    pyautogui.press("enter") # cadastra o produto (botao enviar)
+    # dar scroll de tudo pra cima
+    pyautogui.scroll(5000)
+
 # Passo 5: Repetir o processo de cadastro até o fim
